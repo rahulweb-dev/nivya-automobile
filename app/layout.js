1c4973a6c50b71ce49a97772a0ded3c4ecb2d001
rahulweb-dev <rahulwebdeveloper12@gmail.com>
2025-10-31 17:11:06 +0530

    upadted

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,10 @@ export const metadata = {
   title: 'Nivya Automobiles',
   description:
     'Explore new and used Maruti Suzuki cars in Hyderabad with Nivya Automobiles. Find the best deals, offers, and services.',
+     icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({ children }) {
